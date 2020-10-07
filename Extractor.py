3d0575911b2c0938386304754e1b71f147146399
Luca Baldini <luca.baldini@uniroma1.it>
2020-10-07 23:54:37 +0200

    Add Dataset and Extractor base class and vector spec

--- Extractor.py
+++ Extractor.py
@@ -1,12 +1,20 @@
-import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
+# import pandas as pd
+# from sklearn.preprocessing import MinMaxScaler
 
-class Extractor:
-    def Extract(self, name):
-        data=pd.read_csv(name, sep=",", header=None,names= ["sepal length", "sepal width" , "petal length", "petal width", "class"])
-        scaler = MinMaxScaler()
-        data[['sepal length','sepal width' , 'petal length', 'petal width']] = scaler.fit_transform(data[['sepal length','sepal width' , 'petal length', 'petal width']]) #applico min max scaling
-        data=data.sample(frac=0.5) #frac = fraction of rows to return in the random sample
-        data=data.iloc[:, 1:3]
-        sample = data.values.tolist()
-        return sample # sample = list
+# class Extractor:
+#     def Extract(self, name):
+#         data=pd.read_csv(name, sep=",", header=None,names= ["sepal length", "sepal width" , "petal length", "petal width", "class"])
+#         scaler = MinMaxScaler()
+#         data[['sepal length','sepal width' , 'petal length', 'petal width']] = scaler.fit_transform(data[['sepal length','sepal width' , 'petal length', 'petal width']]) #applico min max scaling
+#         data=data.sample(frac=0.5) #frac = fraction of rows to return in the random sample
+#         data=data.iloc[:, 1:3]
+#         sample = data.values.tolist()
+#         return sample # sample = list
+
+class Extractor(object):
+    
+    def __init__(self,dataset):
+        pass
+        
+    def request(self):
+        raise NotImplementedError
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from Extractor import Extractor
+#from Extractor import Extractor
 from Granulator import Granulator
 from Agent import Agent
 from Metric import Metric
@@ -6,12 +6,47 @@ from Representative import Representative
 from Clustering_MBSAS import Clustering_MBSAS
 from Clustering_K_Means import Clustering_K_Means
 
-extractor1 = Extractor()
+from extraction_strategies import vectorExtractor
 
-obj_clustering_MBSAS = Clustering_MBSAS(3, 0.2, 0.1, 1.1) # Lambda, theta_start ,theta_step, theta_stop
-agent1 = Agent(Granulator, Metric, extractor1, Representative, obj_clustering_MBSAS)
-agent1.execute(3.1,0.5) # S_T, eta
+from pandas import read_csv
+from Datasets import vectorDataset
+from numpy.random import randint
 
-obj_clustering_K_Means = Clustering_K_Means(1,3) #k, k_max
-agent2 = Agent(Granulator, Metric, extractor1, Representative, obj_clustering_K_Means)
-agent2.execute(3.1,0.5) # S_T,  eta
+def reader(path):
+    frame = read_csv(path, sep=",", header=None,names= ["sepal length", "sepal width" , "petal length", "petal width", "class"])
+#    yield frame.iloc[:,0:4].values, frame.iloc[:,4].values 
+    return frame.iloc[:,0:4].values, frame.iloc[:,4].values 
+
+#transform must deal with [pattern, class] = [numpy.ndarray(float), numpy.ndarray(object)]
+def classTrasf():
+    
+    def __call__(self,data):
+        
+        y = map(lambda x: x**2, data)
+        
+        return y
+    
+ext  = vectorExtractor.vectorExtractor(1, 2)
+data1 = vectorDataset.vectorDataset("/home/luca/Documenti/Progetti/E-ABC_v2/eabc_v2/Datasets/iris_data.txt", "iris", reader)
+# data2 = vectorDataset.vectorDataset("/home/luca/Documenti/Progetti/E-ABC_v2/eabc_v2/Datasets/iris_data.txt", "iris", reader, pre_transform=classTrasf)
+
+sliceItem1 = data1[1:10].shuffle()
+sliceItem1 = data1[[1,2,50,100]].shuffle()
+
+count = 0
+for i in range(1,100):
+    sel = randint(0,150, size=randint(2,150)).tolist()
+    x = data1[sel].shuffle()
+    count = count + 1 if x.data == data1[x.indices()].data else count
+    
+print(count)
+# extractor1 = Extractor()
+
+# obj_clustering_MBSAS = Clustering_MBSAS(3, 0.2, 0.1, 1.1) # Lambda, theta_start ,theta_step, theta_stop
+# agent1 = Agent(Granulator, Metric, extractor1, Representative, obj_clustering_MBSAS)
+# agent1.execute(3.1,0.5) # S_T, eta
+
+# obj_clustering_K_Means = Clustering_K_Means(1,3) #k, k_max
+# agent2 = Agent(Granulator, Metric, extractor1, Representative, obj_clustering_K_Means)
+# agent2.execute(3.1,0.5) # S_T,  eta
+
