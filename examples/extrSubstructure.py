fd8e8026a4537e3612305c88d93569e0d4157df9
Luca Baldini <luca.baldini@uniroma1.it>
2020-10-20 17:06:49 +0200

    Not working

--- examples/extrSubstructure.py
+++ examples/extrSubstructure.py
@@ -1,2 +1,25 @@
 # -*- coding: utf-8 -*-
 
+#For graphs
+from Datasets.tudataset import datasets,reader
+from eabc.datasets import graph_nxDataset
+from eabc.extractors import Extractor
+from littleballoffur import RandomWalkWithRestartSampler
+
+def readergraph(path):
+    graphs_nx = reader.tud_to_networkx("COIL-RAG")
+    classes = [g.graph['classes'] for g in graphs_nx]
+    return graphs_nx, classes 
+
+datasets.get_dataset("COIL-RAG")
+gdata3 = graph_nxDataset("/home/luca/Documenti/Progetti/E-ABC_v2/eabc_v2/Datasets/tudataset/COIL-RAG", "Letter-h", readergraph)
+
+sampler = RandomWalkWithRestartSampler()
+sampler.number_of_nodes = 3
+sampler_func= sampler.sample
+
+subgraph_extr = Extractor(sampler_func)
+
+smallD = gdata3[1:10]
+
+subgraphs = subgraph_extr.randomExtractDataset(smallD, 50 )
