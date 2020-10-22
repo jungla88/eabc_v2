60fb966032ea4daf372c15d99353ef5fb4a99910
Luca Baldini <luca.baldini@uniroma1.it>
2020-10-22 14:18:05 +0200

    random walk strategy for Graph_nx type

--- eabc/extractors/randomwalk_restart.py
+++ eabc/extractors/randomwalk_restart.py
@@ -1,15 +1,35 @@
 # -*- coding: utf-8 -*-
 
 from littleballoffur import RandomWalkWithRestartSampler
+from eabc.data import Graph_nx
+import numpy.random
 
+#Interface for Graph_nx Data
+#Accept a Graph_nx type and return a Graph_nx type 
 class extr_strategy:
     
-    def __init__(self):
+    def __init__(self, max_order = 5, seed = None):
         self.sampler = RandomWalkWithRestartSampler()
+        self.max_order = max_order
+        
+        if seed:
+            self._seed = seed;
+            numpy.random.seed(seed)
+            
         
     def __call__(self, data):
         
-        subgraph = self.sampler.sample(data.x)
-        return subgraph
+        upperBoundOrder = self.max_order if data.x.order() > self.max_order else data.x.order()
+        #TODO: else case always exclude highest value from being extracted
+        order = numpy.random.randint(1, upperBoundOrder)  #high value excluded for randint         
+        self.sampler.number_of_nodes = order
+        
+        subgraph = self.sampler.sample(data.x)                       
+
+        subgraphData = Graph_nx()
+        subgraphData.x = subgraph
+        subgraphData.y = data.y
+
+        return subgraphData
         
         
