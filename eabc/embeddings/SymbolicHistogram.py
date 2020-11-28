5ad30d9ca4d08f039a30b3d9690d47d32695712e
jungla88 <luca.baldini@uniroma1.it>
2020-11-28 13:49:19 +0100

    Added for parallel graph embedding (need to be tested)

--- eabc/embeddings/SymbolicHistogram.py
+++ eabc/embeddings/SymbolicHistogram.py
@@ -3,13 +3,42 @@
 from eabc.embeddings import Embedder
 import numpy
 import copy
+from joblib import Parallel, delayed
+
+##Helper functions for Joblib
+def PgetVector(elemDecomposition, alphabetSet,Dissimilarity):
+
+    histogram = numpy.zeros((len(alphabetSet,)))
+    for i,sym in enumerate(alphabetSet): 
+        
+        Diss = Dissimilarity if not None else sym.dissimilarity    
+        j=0
+        for x in elemDecomposition:
+                if Diss(sym.representative,x)<=sym.matchThr:
+                    j+=1
+                    
+        e = copy.deepcopy(j)
+        histogram[i] = e
+    
+    return histogram    
+        
+        
+def JobLibHelper(x,IDs,datasetDecomposed,alphabetSet,Dissimilarity):
+  
+    boolIDs = IDs == x
+    substructIndices = [idx for idx,ids in enumerate(boolIDs) if ids==True]        
+    substrDecomposition = datasetDecomposed[substructIndices]
+    h = PgetVector(substrDecomposition.data,alphabetSet,Dissimilarity)            
+    return (h,x)
+##
+
 
 class SymbolicHistogram(Embedder):
     
-    def __init__(self,Dissimilarity = None, isSymbolDiss = False):
+    def __init__(self,Dissimilarity = None, isSymbolDiss = False,isParallel=True):
         
         self._isSymbolDiss = isSymbolDiss
-        
+        self._isParallel = isParallel
         super().__init__(Dissimilarity)        
 
     @property
@@ -18,7 +47,7 @@ class SymbolicHistogram(Embedder):
     
     def getVector(self, elemDecomposition, alphabetSet):
 
-        self.__sanityCheck()
+#        self.__sanityCheck()
         
         histogram = numpy.zeros((len(alphabetSet,)))
         for i,sym in enumerate(alphabetSet): 
@@ -37,20 +66,34 @@ class SymbolicHistogram(Embedder):
         
     def getSet(self,datasetDecomposed,alphabetSet):
         
+        self.__sanityCheck()
+        
         IDs= numpy.array(datasetDecomposed.indices)
         uniqueIDs = numpy.unique(IDs)
         
         embeddedSet, embeddedIDs =[],[]# []*len(uniqueIDs),[]*len(uniqueIDs)
-        for x in uniqueIDs:
-            
-            boolIDs = IDs == x
-            substructIndices = [idx for idx,ids in enumerate(boolIDs) if ids==True]
-            
-            substrDecomposition = datasetDecomposed[substructIndices]
-            
-            embeddedSet.append(self.getVector(substrDecomposition.data,alphabetSet))
-            embeddedIDs.append(x)
         
+        if self._isParallel:
+            Dissimilarity = self._Dissimilarity if self._isSymbolDiss==False else None
+            output=Parallel(n_jobs=-1)(delayed(JobLibHelper)(x,IDs,datasetDecomposed,alphabetSet,Dissimilarity) for x in uniqueIDs)
+            for i in range(len(output)):
+                embeddedSet.append(output[i][0])
+                embeddedIDs.append(output[i][1])
+            #For debug    
+            assert(len(embeddedSet)==len(embeddedIDs))
+
+        else:
+            
+            for x in uniqueIDs:
+                
+                boolIDs = IDs == x
+                substructIndices = [idx for idx,ids in enumerate(boolIDs) if ids==True]
+                
+                substrDecomposition = datasetDecomposed[substructIndices]
+                self.getVector(substrDecomposition.data,alphabetSet)            
+                embeddedSet.append(self.getVector(substrDecomposition.data,alphabetSet))
+                embeddedIDs.append(x)
+                
         self._embeddedIDs = embeddedIDs
         self._embeddedSet = embeddedSet
         
@@ -60,10 +103,7 @@ class SymbolicHistogram(Embedder):
             raise ValueError("Dissimilarity is not available")
         elif self._isSymbolDiss and self._Dissimilarity:
             print("Warning: Using symbol dissimilarity while global dissimilarity is available")
-            
-            
-            
-            
+
         
         
         
--- examples/graphEmbedding.py
+++ examples/graphEmbedding.py
@@ -11,29 +11,35 @@ from eabc.embeddings import SymbolicHistogram
 import networkx as nx
 import numpy as np
 
-def nodeDissimilarity(a, b):
-        D = 0
-        if(a['labels'] != b['labels']):
-            D = 1
-        return D
+import time
 
-def edgeDissimilarity(a, b):
-        D = 0
-        if(a['labels'] != b['labels']):
-            D = 1
-        return D
+#from numba import njit,jitclass
+
+# @jitclass
+def nodeDissimilarity(a, b):
+    return np.linalg.norm(np.asarray(a['attributes']) - np.asarray(b['attributes'])) / np.sqrt(2)
+
+# @jitclass
+def edgeDissimilarity(a,b):
+    return 0.0
     
 def readergraph(path):
-    graphs_nx = reader.tud_to_networkx("Mutagenicity")
+    graphs_nx = reader.tud_to_networkx("Letter-high")
     classes = [g.graph['classes'] for g in graphs_nx]
     return graphs_nx, classes 
 
 print("Loading...")
-data1 = graph_nxDataset("/home/luca/Documenti/Progetti/E-ABC_v2/eabc_v2/Datasets/tudataset/Mutagenicity", "Mutagenicity", readergraph)
-#Removed not connected graph!
-cleanData = [(g,idx,label) for g,idx,label in zip(data1.data,data1.indices,data1.labels) if nx.is_connected(g)]
+data1 = graph_nxDataset("/home/luca/Documenti/Progetti/E-ABC_v2/eabc_v2/Datasets/tudataset/Letter-high", "LetterH", reader = readergraph)
+#Removed not connected graph and null graph!
+cleanData=[]
+for g,idx,label in zip(data1.data,data1.indices,data1.labels):
+    if not nx.is_empty(g):
+        if nx.is_connected(g):
+            cleanData.append((g,idx,label)) 
+#cleanData = [(g,idx,label) for g,idx,label in zip(data1.data,data1.indices,data1.labels) if not nx.is_empty(g)]
+
 cleanData = np.asarray(cleanData,dtype=object)
-data1 = graph_nxDataset([cleanData[:,0],cleanData[:,2]],"Mutagenicity")
+data1 = graph_nxDataset([cleanData[:,0],cleanData[:,2]],"Letter")
 data1= data1[0:100]
 
 #Test extr indices
@@ -45,7 +51,7 @@ strat = randomwalk_restart.extr_strategy(max_order=6)
 subgraph_extr = Extractor(strat)
 
 print("Extracting...")
-subgraphs = subgraph_extr.randomExtractDataset(data1, 100)
+subgraphs = subgraph_extr.randomExtractDataset(data1, 10000)
 
 Repr= Medoid
 
@@ -56,9 +62,17 @@ granulationStrategy.granulate(subgraphs)
 print("Embedding...")
 expSet = subgraphs.fresh_dpcopy()
 for i,x in enumerate(data1):
-    for j in range(100):
+    for j in range(1000):
         expSet.add_keyVal(data1.to_key(i),subgraph_extr.extract(x))
 
-embeddingStrategy = SymbolicHistogram(graphDist)
-embeddingStrategy.getSet(expSet, granulationStrategy.symbols)
+embeddingStrategy = SymbolicHistogram(graphDist) #Parallel
+embeddingStrategySingle = SymbolicHistogram(graphDist,isParallel=False)
 
+start = time.process_time()
+embeddingStrategy.getSet(expSet, granulationStrategy.symbols)
+print(time.process_time() - start)
+
+embeddingStrategySingle = SymbolicHistogram(graphDist,isParallel=False)
+start = time.process_time()
+embeddingStrategySingle.getSet(expSet, granulationStrategy.symbols)
+print(time.process_time() - start)
