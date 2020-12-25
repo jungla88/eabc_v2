2e6a1eb165fac9728d9d781ec657d052ff530726
jungla88 <luca.baldini@uniroma1.it>
2020-12-25 14:20:04 +0100

    Added varOr for dealing with agent ID

--- eabc/environments/nestedFS.py
+++ eabc/environments/nestedFS.py
@@ -62,7 +62,7 @@ class eabc_Nested:
         #Set if crossovered
         ind1[0]=g_q1[0]
         ind2[0]=g_q2[0]
-        #two point crossover indivividuals are always modified. We edit this slice of genetic code only if condition is valid
+        #two point crossover individuals are always modified. We edit this slice of genetic code only if condition is valid
         if random.random()<indpb:
             for i,(g1,g2) in enumerate(zip(g_ged1,g_ged2),start = 1):
                 ind1[i]=g1
@@ -104,7 +104,7 @@ class eabc_Nested:
         """
 #        mu = [ind[i] for i in range(len(ind))]
         sigmaQ = [10]
-        sigma01 = [0.1 for _ in range(len(ind))]
+        sigma01 = [0.2 for _ in range(len(ind))]
         #Assuming genes  [Q, 01bounded, ... ]
         sigma = sigmaQ+sigma01
         return tools.mutGaussian(ind, mu, sigma, indpb)
@@ -185,7 +185,12 @@ class eabc_Nested:
         
         fitness = f if not np.isnan(f) else 0
         
-        return (fitness,), granulationStrategy.symbols
+        ID = individual.ID
+        symbols = granulationStrategy.symbols
+        for symbol in symbols:
+            symbol.owner = ID
+        
+        return (fitness,), symbols
     
     @staticmethod
     def checkBounds(QMAX):
@@ -251,3 +256,71 @@ class eabc_Nested:
             ranges = ranges + additional
             
         return ranges
+
+    @staticmethod
+    def initAgents(agentCreator,n):
+        c = []
+        for i in range(n):
+            a = agentCreator()
+            a.ID = i
+            c.append(a)
+        
+        return c
+    
+    @staticmethod
+    def varOr(population, toolbox, lambda_, idHistory,cxpb, mutpb):
+        """Part of an evolutionary algorithm applying only the variation part
+        (crossover, mutation **or** reproduction). The modified individuals have
+        their fitness invalidated. The individuals are cloned so returned
+        population is independent of the input population.
+    
+        :param population: A list of individuals to vary.
+        :param toolbox: A :class:`~deap.base.Toolbox` that contains the evolution
+                        operators.
+        :param lambda\_: The number of children to produce
+        :param cxpb: The probability of mating two individuals.
+        :param mutpb: The probability of mutating an individual.
+        :returns: The final population.
+    
+        The variation goes as follow. On each of the *lambda_* iteration, it
+        selects one of the three operations; crossover, mutation or reproduction.
+        In the case of a crossover, two individuals are selected at random from
+        the parental population :math:`P_\mathrm{p}`, those individuals are cloned
+        using the :meth:`toolbox.clone` method and then mated using the
+        :meth:`toolbox.mate` method. Only the first child is appended to the
+        offspring population :math:`P_\mathrm{o}`, the second child is discarded.
+        In the case of a mutation, one individual is selected at random from
+        :math:`P_\mathrm{p}`, it is cloned and then mutated using using the
+        :meth:`toolbox.mutate` method. The resulting mutant is appended to
+        :math:`P_\mathrm{o}`. In the case of a reproduction, one individual is
+        selected at random from :math:`P_\mathrm{p}`, cloned and appended to
+        :math:`P_\mathrm{o}`.
+    
+        This variation is named *Or* because an offspring will never result from
+        both operations crossover and mutation. The sum of both probabilities
+        shall be in :math:`[0, 1]`, the reproduction probability is
+        1 - *cxpb* - *mutpb*.
+        """
+        assert (cxpb + mutpb) <= 1.0, (
+            "The sum of the crossover and mutation probabilities must be smaller "
+            "or equal to 1.0.")
+    
+        offspring = []
+        for _ in range(lambda_):
+            op_choice = random.random()
+            if op_choice < cxpb:            # Apply crossover
+                ind1, ind2 = list(map(toolbox.clone, random.sample(population, 2)))
+                ind1, ind2 = toolbox.mate(ind1, ind2)
+                del ind1.fitness.values
+                offspring.append(ind1)
+                ind1.ID = max(idHistory)+1
+                idHistory.append(ind1.ID)
+            elif op_choice < cxpb + mutpb:  # Apply mutation
+                ind = toolbox.clone(random.choice(population))
+                ind, = toolbox.mutate(ind)
+                del ind.fitness.values
+                offspring.append(ind)
+            else:                           # Apply reproduction
+                offspring.append(random.choice(population))
+    
+        return offspring
