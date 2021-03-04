131f3f8bb7b4040a77b3cd070d8bb4b1554c00be
jungla88 <luca.baldini@uniroma1.it>
2021-03-04 16:36:54 +0100

    added first naive implementation of consensus strat

--- eabc/extras/__init__.py
+++ eabc/extras/__init__.py
@@ -3,3 +3,4 @@ from .gapstatistic import GapStatistic
 from .BSAS import BSAS
 from .modelSelector import eabc_modelGen
 from .rewardingSystem import Rewarder
+from .consensusStrategy import consensusStrategy
