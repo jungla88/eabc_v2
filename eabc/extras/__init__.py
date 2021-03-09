f13b56c1ebe250c6757d3539afbb2bac7e06d303
jungla88 <luca.baldini@uniroma1.it>
2021-03-09 18:05:44 +0100

    added module for ensembleClassifier

--- eabc/extras/__init__.py
+++ eabc/extras/__init__.py
@@ -4,3 +4,4 @@ from .BSAS import BSAS
 from .modelSelector import eabc_modelGen
 from .rewardingSystem import Rewarder
 from .consensusStrategy import consensusStrategy
+from .ensembleClassifier import StackClassifiers
