279909afac8a9cb3f3d6a08b9afef4c98af7bc4a
jungla88 <luca.baldini@uniroma1.it>
2021-03-19 12:15:50 +0100

    Update model selection examples

--- examples/testModelSel.py
+++ examples/testModelSel.py
@@ -2,27 +2,24 @@
 
 import pandas as pd
 from deap import base, creator,tools
-
+import numpy as np
 from eabc.extras import eabc_modelGen
-from eabc.embeddings import SymbolicHistogram
 
 creator.create("FitnessMax", base.Fitness, weights=(1.0,))
 creator.create("Agent", list, fitness=creator.FitnessMax, ID = None)
-log = pd.read_pickle(r'../GREC.pkl')
+log = pd.read_pickle(r'../Letter_mergedAlphabet.pkl')
 
-alphabets = log['ClassAlphabets']
+previousModels = []
+ClassAlphabets = log['ClassAlphabet']
+seed = 0
+rng =np.random.default_rng(seed)
 
-model_generator = eabc_modelGen()
+model_generator = eabc_modelGen(k=5,l=5,seed=rng)
 
-mergedAlphabets = sum(alphabets.values(),[])
 
-randomGeneratedModels = model_generator.createFromSymbols(mergedAlphabets)
-print(len(randomGeneratedModels))
-if randomGeneratedModels:
-    recombinedModels = model_generator.createFromModels(randomGeneratedModels)
-else:
-    recombinedModels= randomGeneratedModels
-    
-mergedModel = randomGeneratedModels+recombinedModels
-     
-    
+randomGeneratedModels = model_generator.createFromSymbols(ClassAlphabets)
+#Create new model from the current and previous generation
+recombinedModels = model_generator.createFromModels(randomGeneratedModels+previousModels)
+
+#Merged random models plus recombined models
+candidateModels = randomGeneratedModels + recombinedModels
