aff3efa481a0f8069acaceaff16d54ed3796f352
jungla88 <luca.baldini@uniroma1.it>
2021-06-11 10:53:35 +0200

    Test without knee search

--- eabc/granulators/__init__.py
+++ eabc/granulators/__init__.py
@@ -2,3 +2,4 @@ from .granule import Granule
 from .granulator import Granulator
 from .AverageLinkageGapSearch import HierchicalAggl
 from .BsasKneeSearch import BsasKneeSearch
+from .Bsas_gralgLike import BsasGralgLike
